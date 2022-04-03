97b0520a58c09719e4bcebfbef0c2ceb067d17c5
JFelipeFloresS <josefelipefloress@gmail.com>
2022-04-03 17:21:58 +0000

    controller and routes update

--- album-controller.js
+++ album-controller.js
@@ -1,5 +1,11 @@
 const Album = require('./models/album');
 
+exports.test = function(req, res) {
+    res.json({
+        message: "Welcome to the top 500 albums of all time. Try adding '/album' at the end of the link to get the full list!"
+    });
+}
+
 exports.addAlbum = function(req, res) {
     let newAlbum = new Album(req.body);
     newAlbum.save(function (err, album) {
@@ -17,10 +23,30 @@ exports.getAlbums = function(req, res) {
     });
 };
 
-exports.getAlbums = function(req, res) {
-
+exports.getAlbum = function(req, res) {
+    Album.findOne({number: req.params.number}, function(err, album) {
+        if (err) {
+            res.status(400).json(err);
+        }
+        res.json(album);
+    }
+    );
 };
 
 exports.updateAlbum = function(req, res) {
-    
+    Album.findOneAndUpdate({number: req.params.number}, req.body, {new: true},function (err, album) {
+      if (err) {
+        res.status(400).json(err);
+      } 
+      res.json(album);
+    }); 
+}
+
+exports.deleteAlbum = function(req, res) {
+    Album.findOneAndRemove({number: req.params.number}, function (err, album) {
+        if (err) {
+          res.status(400).json(err);
+        } 
+        res.json(album);
+      }); 
 }
--- routes.js
+++ routes.js
@@ -4,5 +4,9 @@ const express = require("express"),
 
 router.post('/albums', albumCtrl.addAlbum);
 router.get('/albums', albumCtrl.getAlbums);
+router.get('/albums/:number', albumCtrl.getAlbum);
+router.put('/albums/:number', albumCtrl.updateAlbum);
+router.delete('/albums/:number', albumCtrl.deleteAlbum);
+router.get('/', albumCtrl.test);
 
 module.exports = router;
