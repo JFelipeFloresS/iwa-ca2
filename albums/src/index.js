acfd7dd38da6a28a491bfb71f766c8a5f466318f
Felipe Flores <62604435+JFelipeFloresS@users.noreply.github.com>
2022-05-17 21:56:09 +0100

    Update index.js

--- albums/src/index.js
+++ albums/src/index.js
@@ -4,6 +4,7 @@ import { StrictMode } from 'react';
 import './index.css';
 import { App } from  './App';
 
+// Define app-div as root and renders the React App within it
 const rootElement = document.getElementById('app-div');
 const root = createRoot(rootElement);
 root.render(
