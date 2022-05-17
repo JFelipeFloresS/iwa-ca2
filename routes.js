532cd1cef20d871bbc94d4c7f11c58d1513ef56b
Felipe Flores <62604435+JFelipeFloresS@users.noreply.github.com>
2022-05-17 18:04:36 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,12 +2,15 @@ const express = require("express"),
     router = express.Router(),
     albumCtrl = require("./album-controller");
 
-router.post('/albums', albumCtrl.addAlbum);
-router.get('/albums', albumCtrl.getAlbums);
-router.get('/albums/:number', albumCtrl.getAlbum);
-router.put('/albums/:id', albumCtrl.updateAlbum);
-router.put('/albums', albumCtrl.updateMultipleAlbums);
-router.delete('/albums/:id', albumCtrl.deleteAlbum);
-router.get('/help', albumCtrl.help);
+/**
+*   Routes defined
+*/
+router.post('/albums', albumCtrl.addAlbum);             // INSERT ONE
+router.get('/albums', albumCtrl.getAlbums);             // FIND MANY
+router.get('/albums/:number', albumCtrl.getAlbum);      // FIND ONE 
+router.put('/albums/:id', albumCtrl.updateAlbum);       // UPDATE ONE
+router.put('/albums', albumCtrl.updateMultipleAlbums);  // UPDATE MANY
+router.delete('/albums/:id', albumCtrl.deleteAlbum);    // DELETE ONE
+router.get('/help', albumCtrl.help);                    // HELP
 
 module.exports = router;
